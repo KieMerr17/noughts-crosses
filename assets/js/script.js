62c15b19c81cf47278f5e7ba8be30778eb16c486
KieMerr17 <kieran.merrett@hotmail.com>
2023-01-29 09:46:15 +0000

    feature: if statement added to computer turn to decide wether to play or checkForWin()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
 // Wait for page to finish loading before running game
-// Get the empty cell information and add event listeners to them
+// Get the empty cell information
 const cells = document.getElementsByTagName("td");
 const playerIcon = "X"
 const computerIcon = "O"
 
 document.addEventListener("DOMContentLoaded", function() {
-})
+}) 
 newGame()
 
+
+/**
+ * Function newGame() completely resets the board on loading the page
+ * and following a win or loss of a round
+ */
 function newGame() {
     for (let cell of cells) {
         if (cell.textContent === "X", "O") {
@@ -17,18 +22,34 @@ function newGame() {
     }
 }
 
+/**
+ * This function listens for the players mouse click on the "td" box and if the 
+ * cell is empty, displays the players "X" inside.
+ */
 function playerTurn() {
     for (let cell of cells) {
         cell.addEventListener("click", function() {
             if (this.textContent === "") {
                 this.textContent = playerIcon
                 computerTurn()
-            } 
+            }
         })
     }
 }
 
+/**
+ * This function happens following a players input, the computer generates a random
+ * box to display the computerIcon "O" inside until all boxes are taken.
+ */
 function computerTurn() {
+    let emptyCells = document.querySelectorAll("td:empty");
+    let randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+
+    if (emptyCells.length > 1){
+        randomCell.textContent = "O"
+    } else {
+        console.log("no more boxes")
+    }
 }
 
 function checkForWin() {
@@ -38,8 +59,4 @@ function incrementWins() {
 }
 
 function incrementLosses() {
-}
-
-function resetScores() {
-
 }
