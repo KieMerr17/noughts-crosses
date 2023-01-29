e365d197b72b03837903489a708fcc7088c0e538
KieMerr17 <kieran.merrett@hotmail.com>
2023-01-29 07:43:13 +0000

    add: new game function added and tested on index.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,43 @@
 // Wait for page to finish loading before running game
 // Get the empty cell information and add event listeners to them
+const cells = document.getElementsByTagName("td");
+const playerIcon = "X"
+const computerIcon = "O"
 
 document.addEventListener("DOMContentLoaded", function() {
-    let cells = document.getElementsByTagName("td");
-
-    for (let cell of cells) {
-        cell.addEventListener("click", function() {
-            if (this.textContent === "1") {
-                console.log("no.1 cell clicked")
-            }  
-        })
-    }
 })
+newGame()
 
-function runGame() {
-
+function newGame() {
+    for (let cell of cells) {
+        if (cell.textContent === "X", "O") {
+            cell.textContent = "";
+            playerTurn()
+        }
+    }
 }
 
-function addToCell() {
+function playerTurn() {
+    for (let cell of cells) {
+        cell.addEventListener("click", function() {
+            if (this.textContent === "") {
+                this.textContent = playerIcon
+                computerTurn()
+            } 
+        })
+    }
+}
 
+function computerTurn() {
 }
 
 function checkForWin() {
-
 }
 
 function incrementWins() {
-
 }
 
 function incrementLosses() {
-
 }
 
 function resetScores() {
