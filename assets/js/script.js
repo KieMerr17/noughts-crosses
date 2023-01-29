4ede55c28990e3e140d94c30f0b64606ac8e89b1
KieMerr17 <kieran.merrett@hotmail.com>
2023-01-29 06:16:22 +0000

    add: DOM content loaded and click event listeners added to cells and tested against console.log

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
 // Wait for page to finish loading before running game
 // Get the empty cell information and add event listeners to them
 
-console.log('connected')
+document.addEventListener("DOMContentLoaded", function() {
+    let cells = document.getElementsByTagName("td");
+
+    for (let cell of cells) {
+        cell.addEventListener("click", function() {
+            if (this.textContent === "1") {
+                console.log("no.1 cell clicked")
+            }  
+        })
+    }
+})
 
 function runGame() {
 
