d776cd41a3525571e7b9de984933e61c9bdb7c97
KieMerr17 <kieran.merrett@hotmail.com>
2023-02-07 17:07:38 +0000

    function: add so when form is filled and sent to emailJS, alerts pop up dependant on the outcome

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,30 +1,27 @@
-//Feedback form is hidden until the 'feedback button' is clicked
 const formOuter = document.getElementById("form-outer");
 const feedbackButton = document.getElementById("feedback-button");
 
 formOuter.style.display = "none";
+
 //click event listener for feedback button
 feedbackButton.addEventListener("click", function () {
-  formOuter.style.display = "block";
-  formOuter.scrollIntoView({ behavior: 'smooth' });
+    formOuter.style.display = "block";
+    formOuter.scrollIntoView({ behavior: 'smooth' });
 });
 
-
-/**
- * When clicking submit on the feedback form, this function gets the inputed information
- * and sends the form to emailjs.
- */
-window.onload = function() {
-    document.getElementById('feedback-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs are referenced in the emailjs template
-        emailjs.sendForm('contact_service', 'contact_form', this).then(function() {
-            console.log('SUCCESS!');
-        }, function(error) {
-            console.log('FAILED...', error);
-        });
+// Send form data to email service after user submits the form
+document.getElementById('feedback-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // generate a five digit number for the contact_number variable
+    this.contact_number.value = Math.random() * 100000 | 0;
+    /**Uses the values from the emailJS form template to send the email through emailJS
+     * Following sending of the form, alerts displayed dependant on outcome. 
+     **/
+    emailjs.sendForm('contact_service', 'contact_form', this).then(function() {
+        console.log('SUCCESS!');
+        formOuter.style.display = "none";
+        alert("Thank you for your feedback!");
+    }, function(error) {
+        console.log('FAILED...', error);
     });
-}
-
+});
