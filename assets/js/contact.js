b1d8a493af9b6806eaf66641c9c4b2a23e11b330
KieMerr17 <kieran.merrett@hotmail.com>
2023-02-07 12:59:13 +0000

    function: Add function which connects to emailJS Library and reference in index.html. Use information from Feedback Form for emailJS

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,1 +1,18 @@
-console.log("Hello")
+/**
+ * When clicking submit on the feedback form, this function gets the inputed information
+ * and sends the form to emailjs.
+ */
+window.onload = function() {
+    document.getElementById('feedback-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs are referenced in the emailjs template
+        emailjs.sendForm('contact_service', 'contact_form', this).then(function() {
+            console.log('SUCCESS!');
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+    });
+}
+
